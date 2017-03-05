773bfec1a99083264b5c43dc5e829e7ba9b6a61b
caijw <ygyz_162@126.com>
2017-03-05 13:32:48 +0800

    Initial commit

---
+++ src/com/cjw/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cjw.demo;
+
+/**
+ * Created by Administrator on 2017/3/5 0005.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+
+    }
+}
